465973c04bf2e6f478aa660c5893f88f1b86e86e
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2021-08-27 16:17:36 -0500

    Add npm stuff

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ function fancy_message() {
 
 fancy_message info "Hepno, this script will destroy vscode if installed ;) /s"
 
-sudo pacman -S figlet nodejs-lts-fermium
+sudo pacman -S --noconfirm figlet nodejs npm
 # download vim plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
