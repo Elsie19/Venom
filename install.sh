07d9be5de7db141887ed39cb3301bac5275c698b
Henry Wenger-Stickel <hwengerstickel@protonmail.com>
2021-08-28 11:58:33 -0500

    Add ALE html

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ function fancy_message() {
 
 fancy_message info "Hepno, this script will destroy vscode if installed ;) /s"
 
-sudo pacman -S --noconfirm figlet nodejs npm shellcheck eslint prettier
+sudo pacman -S --noconfirm figlet nodejs npm shellcheck eslint
 # download vim plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
@@ -197,6 +197,7 @@ EOF
 # Install the plugins and CoC stuff
 nvim -c ":PlugInstall"
 nvim -c ":CocInstall coc-sh coc-java coc-html coc-python coc-css coc-omnisharp"
+npm install --save-dev htmlhint
 echo '"diagnostic.displayByAle": true' > $HOME/.config/nvim/coc-settings.json
 if command -v paru &>/dev/null; then
     paru -S nerd-fonts-fira-code csslint
