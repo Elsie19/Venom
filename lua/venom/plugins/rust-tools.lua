4e7923fb6d237c6a15e504cb5703b80ed4dd8e44
Sourajyoti Basak <wiz28@protonmail.com>
2023-01-08 20:51:53 +0530

    refactor(rust-tools): remove rust-analyzer hack (#7)

--- lua/venom/plugins/rust-tools.lua
+++ lua/venom/plugins/rust-tools.lua
@@ -1,16 +1,5 @@
 return {
 	"simrat39/rust-tools.nvim",
 	ft = "rust",
-	config = function()
-		require("rust-tools").setup({
-			server = {
-				settings = {
-					["rust-analyzer"] = {
-						-- HACK: https://github.com/simrat39/rust-tools.nvim/issues/300
-						inlayHints = { locationLinks = false },
-					},
-				},
-			},
-		})
-	end,
+	config = true,
 }
